38bd48735e65cd25bd17cc10ffabbd87dff19c43
udhayakumar <udhayakumargururaj@gmail.com>
2020-05-08 01:05:14 +0530

    git restore --staged <file>...

--- packages/module1/index.js
+++ packages/module1/index.js
@@ -1,4 +1,13 @@
 //Sample file for module one
 
 
-//Sample file for module oneasdfasfads//Sample file for module oneasdfasfads//Sample file for module oneasdfasfads//Sample file for module oneasdfasfads
+//Sample file for module oneasdfasfads//Sample file for module oneasdfasfads//Sample file for module
+
+
+oneasdfasfads//Sample file for module oneasdfasfads
+
+// cekjkfsjfdskjfsdfds dffdsfddf 
+dffddf fd
+df
+dffddfdf
+df
